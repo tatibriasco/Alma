32e7586a0501d5b6ea09fd5357871a6d6435a370
Juan Ignacio Briasco <juanignaciobriasco@gmail.com>
2020-09-18 13:29:27 -0300

    Cambios generales

--- js/funciones.js
+++ js/funciones.js
@@ -7,9 +7,21 @@
         items:2,
         loop:true,
         margin:10,
+        nav:true,
         autoplay:true,
         autoplayTimeout:3000,
-        autoplayHoverPause:true
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
     });
     $('.play').on('click',function(){
         owl.trigger('play.owl.autoplay',[1000])
