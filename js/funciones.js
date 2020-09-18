25f83e0ae6577e816413eba4267c02960ff3d282
Juan Ignacio Briasco <juanignaciobriasco@gmail.com>
2020-09-17 22:11:42 -0300

    Commit inicial de mi repositorio

---
+++ js/funciones.js
@@ -0,0 +1,19 @@
+    $(document).ready(function(){
+      $(".owl-carousel").owlCarousel();
+    });
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:2,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    });
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[1000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    })
