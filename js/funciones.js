8b9749072f145f7cd912ea36ca604da5b8df8e22
Juan Ignacio Briasco <juanignaciobriasco@gmail.com>
2020-10-02 17:21:56 -0300

    +Accesibilidad

--- js/funciones.js
+++ js/funciones.js
@@ -33,3 +33,11 @@ jQuery(document).ready(function(){
     });
 });
 
+/* Defer parsing of JS YouTube (ayudawp.com) */
+function init() {
+    var vidDefer = document.getElementsByTagName('iframe');
+    for (var i=0; i<vidDefer.length; i++) {
+    if(vidDefer[i].getAttribute('data-src')) {
+    vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+    } } }
+    window.onload = init;
